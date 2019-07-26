632470d89b18ceb24e283c68438e53650667a901
ertake <523171376@qq.com>
2019-07-26 17:19:20 +0800

    first commit

---
+++ js/temple-1.0.0.js
@@ -0,0 +1,144 @@
+/**
+ * temple  
+ * 
+ * @author WC 2019/07/17
+ * @version V1.0.1
+ * 
+ * @description ${project.name} 赋值
+ * 		        ${#list:students}<span>循环</span>${#list/} 循环赋值
+ * 
+ */
+var Temple = function(){
+	var _temple = this;
+	this.templeURL = '';
+	this.config = {};
+	this.cacheData = {};
+
+	this.readProperties = function(arg){
+		var obj = {};
+		var pros = arg.split('\n');
+		//FIXME 正则截取替换
+//		var pros = arg.match(/[^#].*$/g);
+
+		pros.forEach(function(_entity){
+			if(_entity.indexOf('#') == 0)return;
+			if(!_entity.trim())return;
+			var temp = _entity.split('=');
+			obj[temp[0].trim()] = temp[1].trim();
+		});
+		return obj;
+	}
+	this.loadProperties = function(){
+		$.ajax({
+		    type: "GET",
+		    url: "config/defaul.properties",
+		    async: false,
+		    dataType: "text",
+		    success: function(arg){
+		    	_temple.config = _temple.readProperties(arg);
+		    }
+		});
+	}();
+	this.initInner = function(){
+		_temple.templeURL = this.config.temple_url;
+	}();
+	
+	this.putDatas = function(str) {
+		return _temple.cellPut(_temple.listPut(str), _temple.cacheData);
+	}
+	
+	this.listPut = function(str){
+		var targetStr = '';
+		//FIXME 添加 ${#if: length eq '3'}  ${#if/} 标签嵌套赋值问题
+//		targetStr = _temple.replaceAll(str, /\$\{#if\:([\W\w](?!)[\W\w])*#if\/\}/g);
+		//单行匹配
+		targetStr = _temple.replaceAll(str, /\$\{#list\:.*(.(?!\$\{#list\/\}).)*#list\/\}/g);
+		//多行匹配  FIXME 截断词后回车换行匹配截断失效问题
+		return _temple.replaceAll(targetStr, /\$\{#list\:([\W\w](?!\$\{#list\/\})[\W\w])*#list\/\}/g);
+	}
+	
+	this.replaceAll = function(str, reg){
+		return str.replace(reg,function(meacher){
+			var _key = meacher.substring(meacher.indexOf('#list:')+6,meacher.indexOf('}')).trim();
+			var temp = meacher.substring(meacher.indexOf('}')+1,meacher.indexOf('${#list/'));
+			var tempData = {};
+			if (_key.indexOf('.')>-1) {
+				var li = _key.split('.');
+				tempData =  _temple.cacheData[li[0]];
+				for (var i = 0; i < li.length; i++) {
+					if (i === 0) {
+						continue;
+					}
+					tempData = tempData[li[i]]
+				}
+			} else {
+				tempData = _temple.cacheData[_key];
+			}
+			var tarStr = '';
+			for (var i=0 ; i< tempData.length ; i++) {
+				tarStr += _temple.cellPut(temp, tempData[i]);
+			}
+			
+			return tarStr;
+		});
+	}
+	
+	this.cellPut = function(str, obj){
+		return str.replace(/\$\{[^}]*}/g,function(meacher){
+			if (typeof obj === 'number' || typeof obj === 'string') {
+				return obj;
+			}
+			meacher = meacher.substring(2,meacher.length-1).trim();
+			if (meacher.indexOf('.')>-1) {
+				var li = meacher.split('.');
+				var temp = obj[li[0]];
+				for (var i = 0; i < li.length; i++) {
+					if (i === 0) {
+						continue;
+					}
+					temp = temp[li[i]]
+				}
+				return temp==undefined?'':temp;
+			}else{
+				return obj[meacher]==undefined?'':obj[meacher];
+			}
+		});		
+	}
+	
+	return {
+		"load" : function(zurl, data) {
+			var res = "";
+			$.ajax({
+			    type: "GET",
+			    url: _temple.templeURL + zurl + ".ht",
+			    async: false,
+			    dataType: "text",
+			    success: function(arg){
+			    	res = arg;
+			    	if(data){
+			    		_temple.cacheData = data;
+			    		res = _temple.putDatas(arg);
+			    	}
+			    }
+			});
+			
+//			var xhr = new XMLHttpRequest();
+//			xhr.open('GET', _temple.templeURL + zurl + '.ht', false);
+//			xhr.onreadystatechange = function(){
+//				if(xhr.readyState === 4 && xhr.status === 200){
+//					res = xhr.responseText;
+//			    	if(data){
+//			    		_temple.cacheData = data;
+//			    		res = _temple.putDatas(xhr.responseText);
+//			    	}
+//				}
+//			}
+//			xhr.send();
+			
+			return res;
+		},
+		"init" : function(option){
+			_temple.templeURL = option.templeURL;
+		}
+	}
+}();
